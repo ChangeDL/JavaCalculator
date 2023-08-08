fe5dad704e0b96178a4427d68ef996a108d65c2a
ChangeDL <douglas.loizzo@gmail.com>
2023-08-08 18:51:30 -0400

    Fixed issue where inputting a non operation would reset the calculator to the beginnning instead of re-inputting operation

--- Calculator.java
+++ Calculator.java
@@ -32,18 +32,11 @@ public class Calculator {
             System.out.println("What Operation would you like to do?");
             System.out.println("type the symbol that best represents it.");
             System.out.print("[ + | - | * | / ]:  ");
-            String operation = input.nextLine();
-
-            if (operation.equals("+") || operation.equals("-") || operation.equals("*") || operation.equals("/")) {
-                sum = result(firstNum, secondNum, operation);
-                System.out.println("Result: " + sum);
-                System.out.print("Would you like to do another problem? (y/n): ");
-                shouldRestart = getYesOrNoInput(input);
-            } else {
-                System.out.println("That is not a valid option for this calculator");
-                System.out.print("Would you like try again? (y/n): ");
-                shouldRestart = getYesOrNoInput(input);
-            }
+            String operation = getOperatorInput(input);
+            sum = result(firstNum, secondNum, operation);
+            System.out.println("Result: " + sum);
+            System.out.print("Would you like to do another problem? (y/n): ");
+            shouldRestart = getYesOrNoInput(input);
         }
         System.out.println("Program ended. Goodbye!");
         input.close();
@@ -61,6 +54,19 @@ public class Calculator {
             }
         }
         return number;
+    }
+
+    public static String getOperatorInput(Scanner scanner) {
+        String operator;
+        while (true) {
+            operator = scanner.next().trim(); // Trim any leading/trailing spaces
+            if (operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("/")) {
+                break; // Exit the loop if input is a valid operator
+            } else {
+                System.out.println("Invalid input. Please enter +, -, *, or /:");
+            }
+        }
+        return operator;
     }
 
     public static boolean getYesOrNoInput(Scanner scanner) {
